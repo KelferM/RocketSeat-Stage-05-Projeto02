8c7f22df2f5af8870b6476fca8e3cb39c5a9841a
Kelfer Marques <114634531+KelferM@users.noreply.github.com>
2022-11-09 22:25:28 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,46 @@
+import { Modal } from './modal.js'
+import { AlertError } from "./alert-error.js"
+import { calculateIMC, notANumber } from "./utils.js"
+
+const form = document.querySelector('form')
+const inputWeight = document.querySelector('#weight')
+const inputHeight = document.querySelector('#height')
+
+form.onsubmit = event => {
+    event.preventDefault()
+
+    const weight = inputWeight.value
+    const height = inputHeight.value
+
+    const weightOrHeightIsNotANumber = notANumber(weight) || notANumber(height)
+
+    if (weightOrHeightIsNotANumber) {
+        AlertError.open()
+        return;
+    }
+
+    const result = calculateIMC(weight, height)
+    displayResultMessage(result)
+}
+
+inputHeight.oninput = () => AlertError.close()
+inputWeight.oninput = () => AlertError.close()
+
+function displayResultMessage(result) {
+    const message = `Seu IMC é de ${result}`
+
+    Modal.message.innerText = message
+    Modal.open()
+}
+
+Modal.buttonClose.onclick = () => {
+  Modal.close()
+}
+
+window.addEventListener('keydown', handleKeydown)
+
+function handleKeydown(event) {
+  if (event.key === 'Escape') {
+      Modal.close()
+  }
+}
